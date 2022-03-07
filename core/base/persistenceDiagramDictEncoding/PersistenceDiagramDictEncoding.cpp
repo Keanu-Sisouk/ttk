c0d2bf6aa4f0a9e8b9b1d8071e9e405792dc670e
Keanu-Sisouk <Keanu.Sisouk@lip6.fr>
2022-03-07 03:01:19 +0100

    calibrating step for test

--- core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
+++ core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
@@ -732,7 +732,7 @@ void PersistenceDiagramDictEncoding::execute(
         // std::cout << "CREATING FEATURES" << std::endl;
         // double factEquiv = sqrt(static_cast<double>(numAtom));
         double factEquiv = numAtom;
-        double step = 1. / (factEquiv * 1e1);
+        double step = 1. / (sqrt(factEquiv) * 1e1);
         for(size_t i = 0 ; i < nDiags ; ++i){
           auto &projForDiag = allProjectionsList[i];
           auto &featuresToAdd = allFeaturesToAdd[i];
