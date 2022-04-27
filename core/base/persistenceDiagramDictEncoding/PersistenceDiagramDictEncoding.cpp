d70beb082946440219bdc7b2169a7f4048d899f0
Keanu-Sisouk <Keanu.Sisouk@lip6.fr>
2022-04-27 04:23:12 +0200

    rectifying (again and again) timer and hyper paramter

--- core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
+++ core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
@@ -581,7 +581,7 @@ void PersistenceDiagramDictEncoding::method(
        && (loss_tab[epoch] / loss_tab[epoch - 1] > 0.99)) {
       if(loss_tab[epoch] < loss_tab[epoch - 1]) {
       //if(true){  
-        if (lag2 == 5){
+        if (lag2 == 10){
           // lag = 0;
           this->printMsg("Loss not decreasing enough");
           if(StopCondition){
