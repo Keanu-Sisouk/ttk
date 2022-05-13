1f869dd030ade6d08bfebd2cb2ddd401fb0ec236
Keanu-Sisouk <Keanu.Sisouk@lip6.fr>
2022-05-13 13:43:49 +0200

    this->maxLag2

--- core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
+++ core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
@@ -651,7 +651,7 @@ void PersistenceDiagramDictEncoding::method(
        && (loss_tab[epoch] / loss_tab[epoch - 1] > 0.99)) {
       if(loss_tab[epoch] < loss_tab[epoch - 1]) {
       //if(true){  
-        if (lag2 == maxLag2){
+        if (lag2 == this->maxLag2){
           // lag = 0;
           this->printMsg("Loss not decreasing enough");
           if(StopCondition){
