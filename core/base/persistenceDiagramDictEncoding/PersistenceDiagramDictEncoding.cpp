5224c82d111b07d5f1aeef3ecf7d10a4fd07d7d5
Keanu-Sisouk <Keanu.Sisouk@lip6.fr>
2022-06-16 18:11:12 +0200

    new atempt fix under diag

--- core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
+++ core/base/persistenceDiagramDictEncoding/PersistenceDiagramDictEncoding.cpp
@@ -1195,13 +1195,15 @@ void PersistenceDiagramDictEncoding::method(
         auto &globalPair = atom[0];
         for(size_t j = 0 ; j < atom.size() ; ++j){
           auto &t = atom[j];
-          if(std::get<6>(t) > std::get<10>(t)){
-            std::get<10>(t) = std::get<6>(t);
-          }
           
           if(std::get<10>(t) > std::get<10>(globalPair)){
             std::get<10>(t) = std::get<10>(globalPair);
           }
+
+          if(std::get<6>(t) > std::get<10>(t)){
+            std::get<10>(t) = std::get<6>(t);
+          }
+          
         }
       }
       this->printMsg("Computed 2nd opt for epoch " + std::to_string(epoch),
