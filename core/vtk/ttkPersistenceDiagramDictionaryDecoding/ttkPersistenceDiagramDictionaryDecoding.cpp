dbe51c21a70947393ef6ac338f33e340a28e0c4c
Keanu-Sisouk <Keanu.Sisouk@lip6.fr>
2023-06-14 16:46:36 +0200

    Clang formatting for CI

--- core/vtk/ttkPersistenceDiagramDictionaryDecoding/ttkPersistenceDiagramDictionaryDecoding.cpp
+++ core/vtk/ttkPersistenceDiagramDictionaryDecoding/ttkPersistenceDiagramDictionaryDecoding.cpp
@@ -111,7 +111,8 @@ int ttkPersistenceDiagramDictionaryDecoding::RequestData(
     for(size_t i = 0; i < nDiags; ++i) {
       std::string name{"Atom"};
       zeroPad(name, nDiags, i);
-      inputWeights[i] = vtkDataArray::SafeDownCast(weightsVTK->GetColumnByName(name.c_str()));
+      const auto colName = name.c_str();
+      inputWeights[i] = vtkDataArray::SafeDownCast(weightsVTK->GetColumnByName(colName));
     }
   }
 
