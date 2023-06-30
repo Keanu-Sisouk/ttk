1b11be216ed4accb5c8e2a0df37de1bbd568768a
Keanu-Sisouk <Keanu.Sisouk@lip6.fr>
2023-06-30 16:44:13 +0200

    [InitDictPersDiagram base] correction

--- core/base/persistenceDiagramClustering/PersistenceDiagramClustering.h
+++ core/base/persistenceDiagramClustering/PersistenceDiagramClustering.h
@@ -130,7 +130,7 @@ namespace ttk {
     bool UseInterruptible{true};
     double Alpha{1.0};
     bool UseAdditionalPrecision{false};
-    double DeltaLim{0.0001};
+    double DeltaLim{0.01};
     double Lambda{1.0};
     double TimeLimit{999999};
     double NonMatchingWeight = 1.0;
